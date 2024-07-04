617f533d9c31d3fd7319f676c88326c7047e9dae
delafuentej <jfuentelucas@gmail.com>
2024-07-04 17:47:29 +0200

    init: structure folder

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/react-in-jsx-scope */
+
+import { Text, View } from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+        <Text>ComponentsApp</Text>
+    </View>
+  );
+};
