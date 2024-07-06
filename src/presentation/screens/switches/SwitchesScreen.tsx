fbe312a69c97f57f9fbd55ff2db9a49bf6557c2c
delafuentej <jfuentelucas@gmail.com>
2024-07-06 15:21:53 +0200

    custom switches

--- src/presentation/screens/switches/SwitchesScreen.tsx
+++ src/presentation/screens/switches/SwitchesScreen.tsx
@@ -4,12 +4,41 @@
 
 import { Title } from '../../components/ui/Title';
 import { CustomView } from '../../components/ui/CustomView';
+import { useState } from 'react';
+import { Card } from '../../components/ui/Card';
+import { CustomSwitch } from '../../components/ui/CustomSwitch';
 
 
 export const SwitchesScreen = () => {
+    // const [isEnabled, setIsEnabled] = useState(false);
+    // const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    const [state, setState] = useState({
+        isActive: true,
+        isHandsome: false,
+        isHappy: true,
+    })
+
   return (
     <CustomView style={{marginTop: 100, paddingHorizontal:10}}>
         <Title text='SwitchesScreen'/>
+        <Card>
+            <CustomSwitch
+                isOn={state.isActive}
+                onChange={(value)=> setState({...state, isActive: value})}
+                text='Activ'
+            />
+             <CustomSwitch
+                isOn={state.isHandsome}
+                onChange={(value)=> setState({...state, isHandsome: value})}
+                text='Handsome'
+            />
+             <CustomSwitch
+                isOn={state.isHappy}
+                onChange={(value)=> setState({...state, isHappy: value})}
+                text='Happy'
+            />
+
+        </Card>
         
     </CustomView>
   );
