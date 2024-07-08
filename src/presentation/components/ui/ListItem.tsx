59050d302e080ceb8a6942d8014a863c5df01a58
delafuentej <jfuentelucas@gmail.com>
2024-07-08 08:50:25 +0200

    image infinitescroll

---
+++ src/presentation/components/ui/ListItem.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/react-in-jsx-scope */
+
+import {Image, Text } from 'react-native';
+import { colors } from '../../../config/theme/globalStyles';
+
+interface Props {
+    number:number;
+}
+
+export const ListItem = ({number}: Props) => {
+  return (
+    <Image 
+        source={{ uri: `https://picsum.photos/id/${number}/200/300`}}
+        style={{ height: 400, width: '100%'}}
+    />
+        // <Text style={{ height: 400, backgroundColor: colors.primary, color:'white', fontWeight: 'bold', fontSize: 50}}>
+        // {number}
+        // </Text>
+  );
+};
--- src/presentation/screens/ui/InfiniteScrollScreen.tsx
+++ src/presentation/screens/ui/InfiniteScrollScreen.tsx
@@ -2,34 +2,42 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable prettier/prettier */
 
-import { Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
 import { Title } from '../../components/ui/Title';
 import { useState } from 'react';
 import { CustomView } from '../../components/ui/CustomView';
 import { FlatList } from 'react-native-gesture-handler';
 import { colors } from '../../../config/theme/globalStyles';
+import { ListItem } from '../../components/ui/ListItem';
 
 export const InfiniteScrollScreen = () => {
     const[numbers, setNumbers] = useState([0,1,2,3,4,5,6,7,8,9,10]);
 
-    const loadMore=()=>{
+    const loadMore=() => {
         const newArray = Array.from({length: 10},(_,i) =>numbers.length + i);
 
-        setTimeout(()=>{
+        setTimeout(() => {
             setNumbers([...numbers, ...newArray]);
-        },2000);
+        },5000);
     };
   return (
-    <CustomView margin>
+    <View style={{backgroundColor:'black'}}>
         <Title text='InfiniteScroll Screen' safe/>
         <FlatList
             data={numbers}
             onEndReached={loadMore}
-            //para que carge los items antes de llegar al final => 
+            //para que carge los items antes de llegar al final => onEndReachedThreshold
             onEndReachedThreshold={0.6}
             keyExtractor={(item)=>item.toString()}
-            renderItem={({item})=><Text style={{ height: 400, backgroundColor: 'black', color:'white', fontWeight: 'bold', fontSize: 50}}>{item}</Text>}
+            renderItem={({item}) => <ListItem number={item}/>}
+            ListFooterComponent={()=>{
+                <View style={{height: 150, justifyContent:'center'}}>
+                    <ActivityIndicator size={60} color={colors.primary} />
+
+                </View>;
+            }}
+            // renderItem={({item})=>{/* <Text style={{ height: 400, backgroundColor: 'black', color:'white', fontWeight: 'bold', fontSize: 50}}>{item}</Text>} */}
         />
-    </CustomView>
+    </View>
   );
 };
